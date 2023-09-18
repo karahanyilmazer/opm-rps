fc1abf007892e91b57be7d77e9f4b777160de1a8
Karahan Yilmazer <karahan.yilmazer@tum.de>
2023-09-19 00:38:08 +0200

    refactor: Use new data loading scheme

--- preprocessing_single.py
+++ preprocessing_single.py
@@ -7,24 +7,27 @@ Preprocess OPM data from a single run.
 """
 # %% Import packages
 # !%matplotlib qt
+# !%load_ext autoreload
+# !%autoreload 2
 import os
 import sys
 
 sys.path.insert(0, r'C:\Files\Coding\Python\Neuro\eeg_classes')
+os.environ[
+    'SUBJECTS_DIR'
+] = r'C:\Files\Coding\Python\Neuro\data\Gesture\Nottingham\MRI\Segmentation'
 
 import matplotlib.pyplot as plt
 import mne
 import numpy as np
-from matplotlib._pylab_helpers import Gcf
 from scipy.stats import zscore
 from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from src.base.EEG import EEG
 from src.preprocessing.FeatureExtractor import FeatureExtractor
-from src.utils.DataLoader import DataLoader
 
 # High-DPI monitor settings
 if 'qApp' not in vars():
@@ -33,29 +36,25 @@ if 'qApp' not in vars():
     qApp = QtWidgets.QApplication(sys.argv)
     plt.matplotlib.rcParams['figure.dpi'] = qApp.desktop().physicalDpiX()
 # %%
-loader = DataLoader()
-
-data_dir = r'C:\Files\Coding\Python\Neuro\data\Gesture\Nottingham\Rock Paper Scissors'
-day = '20230623'  # or '20230622'
-acq_time_dict = {
-    'noise_1': '095228',
-    'noise_2': '100008',
-    'noise_3': '100245',
-    'run_1': '102814',
-    'run_2': '104104',
-    'run_3': '105342',  # has some data from run_4 after 681.499 s
-    'run_4': '110808',
-    'rest': '112029',
+data_dict = {
+    'data_dir': r'C:\Files\Coding\Python\Neuro\data',
+    'paradigm': 'Gesture',
+    'dataset': 'Nottingham Gesture',
+    'device': 'OPM',
+    'subject': 11766,
+    'session': 20230623,  # or 20230622
+    'run': 'run_3',
 }
 
-acq_time_key = 'run_1'
-raw, events, event_id = loader.get_meg_data(data_dir, day, acq_time_dict[acq_time_key])
-# Crop the end of Run 3 as it includes the beginning of Run 4
-if acq_time_key == 'run_3':
-    raw.crop(tmax=681.499)
-    idx = np.where(events[:, 2] == 255)[0][1] + 1
-    events = events[:idx, :]
+cropping = (0, None)
+if data_dict['run'] == 'run_3':
+    # Crop the end of Run 3 as it includes the beginning of Run 4
+    cropping = (0, 681.449)
 
+fmin, fmax = None, 400
+tmin, tmax = -0.5, 2.1
+events = ('cue_1', 'cue_2', 'cue_3')
+# events = None
 combined_bads = [
     'FR[Z]',
     'FZ[X]',
@@ -69,6 +68,7 @@ combined_bads = [
     'KF[Y]',
     'LB[X]',
     'LB[Y]',
+    'LF[X]',
     'LN[X]',
     'LN[Y]',
     'LN[Z]',
@@ -80,26 +80,16 @@ combined_bads = [
     'MU[Z]',
 ]
 
-raw.info['bads'] = combined_bads
-
-notch_freqs = np.arange(50, 600, 50)
-notch_freqs = np.insert(notch_freqs, 5, 277)
-raw.notch_filter(notch_freqs)
-
-x_axis_channels = [ch for ch in raw.ch_names if '[X]' in ch]
-y_axis_channels = [ch for ch in raw.ch_names if '[Y]' in ch]
-z_axis_channels = [ch for ch in raw.ch_names if '[Z]' in ch and 'Trigger' not in ch]
-
-raws = dict()
-raws['x'] = raw.copy().pick(x_axis_channels)
-raws['y'] = raw.copy().pick(y_axis_channels)
-raws['z'] = raw.copy().pick(z_axis_channels)
-del raw
-
-# %%
-# Plotting for sanity check
-# raws['x'].plot(events=events, event_id=event_id, scalings='auto')
-# raws['x'].compute_psd().plot(picks='data', exclude='bads')
+meg = EEG(
+    data_dict,
+    bp_filt=(fmin, fmax),
+    epoching=(tmin, tmax),
+    cropping=cropping,
+    events=events,
+    apply_notch=True,
+    bad_chs=combined_bads,
+    logger_name='meg_analysis',
+)
 
 # %%
 # First look:
@@ -116,25 +106,42 @@ del raw
 # Sus channels (Run 2): G0[X], HJ[X], LF[X], K9[Y], KC[Z]
 
 # Bad channels (Run 3): LN[X, Y, Z], K9[X], FZ[X], MV[X, Y, Z], LB[X, Y], LP[X, Y], HJ[X, Y], KC[Y, Z], KE[Y], KF[Y], MU[Z]
-# Sus channels (Run 3): HO[Z]
+# Sus channels (Run 3): HO[Z], LF[X]
 
 # Bad channels (Run 4): LN[X, Y, Z], K9[X], FZ[X], LB[X], HJ[X], LP[X], KF[Y], MV[Y, Z], KC[Y, Z], KE[Y], HF[Y], LB[Y], MU[Z]
 # Sus channels (Run 4): G0[X], K9[Z], LM[Z], LP[Z]
+
+x_axis_channels = [ch for ch in meg.raw.ch_names if '[X]' in ch]
+y_axis_channels = [ch for ch in meg.raw.ch_names if '[Y]' in ch]
+z_axis_channels = [ch for ch in meg.raw.ch_names if '[Z]' in ch and 'Trigger' not in ch]
+
+raws = dict()
+raws['x'] = meg.raw.copy().pick(x_axis_channels)
+raws['y'] = meg.raw.copy().pick(y_axis_channels)
+raws['z'] = meg.raw.copy().pick(z_axis_channels)
+# %%
+# Plotting for sanity check
+# raws['x'].plot(events=events, event_id=event_id, scalings='auto')
+raws['x'].compute_psd().plot(picks='data', exclude='bads', average=True)
 # %%
 # Define events of interest
 event_id_subset = {'cue_1': 1, 'cue_2': 2, 'cue_3': 4}
+tmin, tmax = -0.5, 2.1
 
 # Create epochs
 epochs = dict()
 epochs['x'] = mne.Epochs(
-    raws['x'], events, event_id_subset, tmin=-0.5, tmax=2.1, baseline=None
+    raws['x'], meg.event_arr, event_id_subset, tmin=tmin, tmax=tmax, baseline=None
 )
 epochs['y'] = mne.Epochs(
-    raws['y'], events, event_id_subset, tmin=-0.5, tmax=2.1, baseline=None
+    raws['y'], meg.event_arr, event_id_subset, tmin=tmin, tmax=tmax, baseline=None
 )
 epochs['z'] = mne.Epochs(
-    raws['z'], events, event_id_subset, tmin=-0.5, tmax=2.1, baseline=None
+    raws['z'], meg.event_arr, event_id_subset, tmin=tmin, tmax=tmax, baseline=None
 )
+
+# %%
+epochs['x'].plot(scalings='auto', butterfly=True)
 
 # %%
 _, axs = plt.subplots(3, 1)
@@ -145,40 +152,45 @@ epochs_spectrum['x']['cue_2'].plot(picks='data', exclude='bads', axes=axs[1])
 epochs_spectrum['x']['cue_3'].plot(picks='data', exclude='bads', axes=axs[2])
 
 # %%
+data_cov = mne.compute_covariance(epochs, tmin=0.01, tmax=0.25, method="empirical")
+noise_cov = mne.compute_covariance(epochs, tmin=tmin, tmax=0, method="empirical")
+data_cov.plot(epochs.info)
+
+# %%
 fe = FeatureExtractor(epochs['x'])
 X_var = fe.get_var_feat(epochs['x'].get_data(), epochs['x'].ch_names)
-X_bp = fe.get_bp_feat(epochs['x'].get_data(), epochs['x'].ch_names)
+X_bp_x = fe.get_bp_feat(epochs['x'].get_data(), epochs['x'].ch_names)
+fe = FeatureExtractor(epochs['y'])
+X_bp_y = fe.get_bp_feat(epochs['y'].get_data(), epochs['y'].ch_names)
+fe = FeatureExtractor(epochs['z'])
+X_bp_z = fe.get_bp_feat(epochs['z'].get_data(), epochs['z'].ch_names)
+
 y = epochs['x'].events[:, 2]
 
 # %%
-X = X_bp
+# X = X_bp_x
+X = np.concatenate([X_bp_x, X_bp_y, X_bp_z], axis=1)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
 cv_train_scores = []
 cv_test_scores = []
+
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+ss = StandardScaler()
+svm = SVC(gamma='auto')
+
+pipe = make_pipeline(ss, svm)
 
 for train_index, test_index in skf.split(X, y):
     X_train, X_test = [X[i] for i in train_index], [X[i] for i in test_index]
     y_train, y_test = [y[i] for i in train_index], [y[i] for i in test_index]
 
-    svm = SVC(gamma='auto')
-
-    # You can train and test your model within this loop.
-    # print("TRAIN:", train_index, "TEST:", test_index)
-    # print("X_train:", X_train)
-    # print("X_test:", X_test)
-    # print("y_train:", y_train)
-    # print("y_test:", y_test)
-    svm.fit(X_train, y_train)
-    # print(svm.predict(X_train))
-    # print(svm.predict(X_test))
-    cv_train_scores.append(svm.score(X_train, y_train))
-    cv_test_scores.append(svm.score(X_test, y_test))
-    # print("-" * 40)
+    pipe.fit(X_train, y_train)
+    cv_train_scores.append(np.round(pipe.score(X_train, y_train), 3))
+    cv_test_scores.append(np.round(pipe.score(X_test, y_test), 3))
 
 print('TRAIN')
 print(cv_train_scores)
