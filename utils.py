314b8a29c169bfd247f174627cbc22940abc0695
Karahan Yilmazer <karahan.yilmazer@tum.de>
2024-02-11 18:05:46 +0100

    feat: Add functions to set figure settings

--- utils.py
+++ utils.py
@@ -11,10 +11,37 @@ import os
 import re
 from typing import List, Tuple
 
+import matplotlib.pyplot as plt
 import mne
 import numpy as np
 import pandas as pd
+import scienceplots
+from matplotlib.colors import LinearSegmentedColormap
 from mne.io.constants import FIFF
+from yaml import safe_load
+
+
+def set_style(notebook=True, grid=False):
+    # Set the style to science
+    args = ['science', 'no-latex']
+    if grid:
+        args.append('grid')
+    if notebook:
+        args.append('notebook')
+    plt.style.use(args)
+
+
+def set_fig_dpi():
+    # Set the figure dpi to 260
+    plt.matplotlib.rcParams['figure.dpi'] = 260
+
+
+def get_cmap(name):
+    # Load the (parula) cmap
+    with open(f'{name}.yaml', 'r') as file:
+        cmap = safe_load(file)['cmap']
+        cmap = LinearSegmentedColormap.from_list(name, cmap)
+    return cmap
 
 
 def read_old_cMEG(filename: str) -> np.ndarray:
